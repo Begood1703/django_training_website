f5c28e1a9cb301cf8d540783f3a16c9e3fa1d023
begood1703 <begood1703@gmail.com>
2022-10-28 14:18:45 +0300

    second_comit

--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
@@ -115,6 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / "landingpagewebsite/static/",
+]
+
 
 STATIC_URL = 'static/'
 
